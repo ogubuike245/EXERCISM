815ab497ee8604f18eab88f1bf8a08747995653d
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2023-03-20 19:31:39 +0100

    EXERCISM

---
+++ javascript/hello-world/hello-world.js
@@ -0,0 +1,11 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+// Write a function that returns the string "Hello, World!".
+// Run the test suite and make sure that it succeeds.
+// Submit your solution and check it at the website.
+export function hello() {
+  return "Hello, World!";
+}
